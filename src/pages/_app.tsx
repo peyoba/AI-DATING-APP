c6af1f8f5d7acc29944d09a70726b4f4d703b929
peyoba <396217818@qq.com>
2024-12-09 00:15:34 +0800

    Add user context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css';
+import { UserProvider } from '../contexts/UserContext';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
 export default MyApp;
